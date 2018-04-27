a4bef87102d7160d11ba3194464a30baa5e47ff9
Mary Trollinger <marytrollinger678@gmail.com>
2018-04-27 01:53:20 +0000

    add changes

--- fbscape.py
+++ fbscape.py
@@ -11,10 +11,14 @@ def main():
         else    # otherwise
             break   # break out of while loop
 
-    photos = {}    # empty space for adding photos per element in the array
+    photos = {}    # empty list for adding photos
         while True:
             # When you read a person's Photos you can also include an optional type parameter with the value uploaded to get only the list of photos that a person has uploaded
             GET /{user-id}/photos?type=uploaded     # getting the list of photos from the user even if they're mobile photos from different albums
+                "userphotodata": [],
+                
+                multipart/form-data # uploading more photos at a time instead of just one
+                
             # when previous photo is the same than one before don't continue
             if photos = "no more photos when they pull anymore":    # need something to say I ran out of photos
                 break
@@ -23,9 +27,16 @@ def main():
             # create fake email so you can create fake facebook
 
 
-img = urllib2.urlopen(settings.STATICMAP_URL.format(**data))    # downloading images after getting them (https://stackoverflow.com/questions/13137817/how-to-download-image-using-requests/33866125)
+img = urllib2.urlopen(settings.STATICMAP_URL.format(**data)) # downloading images after getting them (https://stackoverflow.com/questions/13137817/how-to-download-image-using-requests/33866125)
 with open(path, 'w') as f:      # context processor "with", another technique for opening a file
     f.write(img.read())     # write the file and goes back to read it
 
+    GET /{user-id}/photos?type=uploaded
+
     # python has garbage collector so you don't need malloc
     # https://developers.facebook.com/docs/graph-api/reference/user/photos/   (this is the api for facebook)
+
+
+
+
+
